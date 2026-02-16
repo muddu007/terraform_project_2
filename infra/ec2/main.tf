49d39ca94916e6c113be820bb511b286abe41034
muddu007 <muddu.skh@gmail.com>
2026-02-16 12:48:51 +0530

    fixed keys

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -9,7 +9,7 @@ variable "user_data_install_apache" {}
 variable "ec2_sg_name_for_python_api" {}
 
 output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "ssh -i /home/linux/Desktop/Ci_cd/project_2/terraform-jenkins/ ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
+  value = format("%s%s", "ssh -i /home/mudassar ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
 }
 
 output "dev_proj_1_ec2_instance_id" {
